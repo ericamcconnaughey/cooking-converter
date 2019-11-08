29bd9981604690a71af734ad91a20f6824322acf
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-11-08 15:00:25 -0600

    add 4 more conversion functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Gallons to Liters
 function convertGallonsToLiters(gallons) {
   return (gallons * 3.8);
 }
@@ -6,8 +7,7 @@ var inputGallons = prompt("Enter a measurement in gallons:");
 
 var gallonsInt = parseInt(inputGallons);
 
-
-
+//Liters to Gallons
 function convertLitersToGallons(liters) {
   return (liters / 3.8);
 }
@@ -15,11 +15,38 @@ function convertLitersToGallons(liters) {
 var inputLiters = prompt("Enter a measurement in liters:");
 var litersInt = parseInt(inputLiters);
 
-
-
-function convertCupstoMillileters(cups) {
+//Cups to Milliliters
+function convertCupsToMillileters(cups) {
 	return (cups * 240)
 }
+
+var inputCups = prompt("Enter a measurement in cups:");
+var cupsInt = parseInt(inputCups);
+
+//Milliliters to Cups
+function convertMilliletersToCups(mls) {
+	return (mls / 240)
+}
+
+var inputMls = prompt("Enter a measurement in milliliters:");
+var mlsInt = parseInt(inputMls);
+
+//Teaspoons to Milliliters
+function convertTeaspoonsToMillileters(tsps) {
+	return (tsps / 5)
+}
+
+var inputTeaspoons = prompt("Enter a measurement in teaspoons:");
+var teaspoonsInt = parseInt(inputTeaspoons);
+
+//Milliliters to Teaspoons
+function convertMilliletersToTeaspoons(mls) {
+	return (mls / 5)
+}
+
+var inputMls = prompt("Enter a measurement in milliliters:");
+var mlsInt = parseInt(inputMls);
+
 
 
 
