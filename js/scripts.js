fe9835466414704e50bc196ee46c584c7194642f
Erica McConnaughey <ericamcconnaughey@gmail.com>
2019-11-21 14:19:54 -0600

    add jQuery forms for each conversion type. add Bootstrap styles to forms.

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,116 @@
+// Back-End
+
 //Gallons to Liters
 function convertGallonsToLiters(gallons) {
   return (gallons * 3.8);
 }
-
-var inputGallons = prompt("Enter a measurement in gallons:");
-
-var gallonsInt = parseInt(inputGallons);
 
 //Liters to Gallons
 function convertLitersToGallons(liters) {
   return (liters / 3.8);
 }
 
-var inputLiters = prompt("Enter a measurement in liters:");
-var litersInt = parseInt(inputLiters);
-
 //Cups to Milliliters
 function convertCupsToMillileters(cups) {
 	return (cups * 240)
 }
-
-var inputCups = prompt("Enter a measurement in cups:");
-var cupsInt = parseInt(inputCups);
 
 //Milliliters to Cups
 function convertMilliletersToCups(mls) {
 	return (mls / 240)
 }
 
-var inputMls = prompt("Enter a measurement in milliliters:");
-var mlsInt = parseInt(inputMls);
-
 //Teaspoons to Milliliters
 function convertTeaspoonsToMillileters(tsps) {
-	return (tsps / 5)
+	return (tsps * 5)
 }
-
-var inputTeaspoons = prompt("Enter a measurement in teaspoons:");
-var teaspoonsInt = parseInt(inputTeaspoons);
 
 //Milliliters to Teaspoons
 function convertMilliletersToTeaspoons(mls) {
 	return (mls / 5)
 }
 
+
+
+
+//Front-End
+$(document).ready(function() {
+	$("form#galToLit").submit(function(event) {
+	event.preventDefault();
+	var gallons = parseInt($("#gallons").val());
+	
+	var result = convertGallonsToLiters(gallons);
+	$("#galToLit-output").text(result);
+	console.log(result);
+})
+
+$("form#litToGal").submit(function(event) {
+	event.preventDefault();
+	var liters = parseInt($("#liters").val());
+	
+	var result = convertLitersToGallons(liters);
+	$("#litToGal-output").text(result);
+	console.log(result);
+})
+
+$("form#cupToMill").submit(function(event) {
+	event.preventDefault();
+	var cups = parseInt($("#cups").val());
+	
+	var result = convertCupsToMillileters(cups);
+	$("#cupToMill-output").text(result);
+	console.log(result);
+})
+
+$("form#millToCups").submit(function(event) {
+	event.preventDefault();
+	var milliliters = parseInt($("#milliliters1").val());
+	
+	var result = convertMilliletersToCups(milliliters);
+	$("#millToCups-output").text(result);
+	console.log(result);
+})
+
+$("form#tspToMill").submit(function(event) {
+	event.preventDefault();
+	var teaspoons = parseInt($("#teaspoons").val());
+	
+	var result = convertTeaspoonsToMillileters(teaspoons);
+	$("#tspToMill-output").text(result);
+	console.log(result);
+})
+
+$("form#millToTsps").submit(function(event) {
+	event.preventDefault();
+	var milliliters = parseInt($("#milliliters2").val());
+	
+	var result = convertMilliletersToTeaspoons(milliliters);
+	$("#millToTsps-output").text(result);
+	console.log(result);
+})
+
+/*
+var inputCups = prompt("Enter a measurement in cups:");
+var cupsInt = parseInt(inputCups);
+
+
+
+var inputMls = prompt("Enter a measurement in milliliters:");
+var mlsInt = parseInt(inputMls);
+
+
+
+var inputTeaspoons = prompt("Enter a measurement in teaspoons:");
+var teaspoonsInt = parseInt(inputTeaspoons);
+
+
+
 var inputMls = prompt("Enter a measurement in milliliters:");
 var mlsInt = parseInt(inputMls);
 
 
 
 
-alert(convertLitersToGallons(litersInt));
+alert(convertLitersToGallons(litersInt));*/
+
+});
